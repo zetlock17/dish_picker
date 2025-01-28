683d636957791013b94919e4ba55536b53b7b8e0
zetlock17 <95153376+zetlock17@users.noreply.github.com>
2025-01-29 09:18:21 +1000

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/components/header.scss';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>start</h1>
+        </div>
+    );
+};
+
+export default App;
